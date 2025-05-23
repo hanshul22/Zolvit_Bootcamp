a0d2b2deac1bceb4386533c57205b3c26f4b075a
hanshul22 <hanshulkumawat22@gmail.com>
2025-05-23 14:01:50 +0530

    Server setup completed day 3 task (server.js)

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,21 @@
-const hello = require("./Hello");
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+require('dotenv').config()
 
-const printMessage = new hello();
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head><title>Home</title></head>
+      <body><h1>Welcome to the Homepage</h1></body>
+    </html>
+  `);
+});
 
-console.log(printMessage.hello());
+app.get('/data', (req, res) => {
+  res.json({ message: 'Hello from the server' });
+});
 
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
