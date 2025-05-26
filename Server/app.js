d039200736bf47add1ac65cb7db87cd494849c72
hanshul22 <hanshulkumawat22@gmail.com>
2025-05-26 13:46:37 +0530

    completed day 5(app.js) chalk to color terminal

---
+++ Server/app.js
@@ -0,0 +1,8 @@
+// app.js
+console.log('Starting app...');
+import chalk from 'chalk';
+
+console.log(chalk.green('✅ Success! Your terminal is colored with green.'));
+console.log(chalk.blue('ℹ️  Info: This is a blue-colored log.'));
+console.log(chalk.red('❌ Error: This is a red-colored log.'));
+
